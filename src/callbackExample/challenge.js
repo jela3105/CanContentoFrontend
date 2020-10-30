b9ca39c5f172452e0e909445ba62d1fa589be259
jela3105 <jela3105@gmail.com>
2020-10-29 23:47:59 -0600

    First try to fetch data

--- src/callbackExample/challenge.js
+++ src/callbackExample/challenge.js
@@ -2,6 +2,7 @@
 //Este se usa con callbacks
 
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/'
 
 function fetchData(url_api, callback){
     let xhttp = new XMLHttpRequest();
@@ -19,3 +20,17 @@ function fetchData(url_api, callback){
     }
     xhttp.send();
 }
+
+//hacer peticion a la api, obtener cuantos elementos de personajes tiene, acceder al primer personaje, obtener ubicacion del personaje, obtener dimension
+fetchData(API, function(error1, data1){
+    if(error1) return console.error(error1);
+    fetchData(API + data1.results[0].id, function(error2, data2){
+        if(error2) return console.error(error2)
+        fetchData(data2.origin.url, function(error3, data3){
+            if(error3) return console.error(error3);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        })
+    });
+})
