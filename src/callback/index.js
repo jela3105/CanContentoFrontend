a72d89b59bd5a60e3c50a613988936a6390e4d08
jela3105 <jela3105@gmail.com>
2020-10-29 22:42:42 -0600

    add second callback

--- src/callback/index.js
+++ src/callback/index.js
@@ -6,4 +6,18 @@ function calc(num1, num2, callback){
     return callback(num1, num2);
 }
 
-console.log(calc(2,2,sum));
+console.log(calc(2,2,sum)); 
+
+function date(callback){
+    console.log(new Date)
+    setTimeout(function(){
+        let date = new Date;
+        callback(date);
+    },5000)
+}
+
+function printDate(){
+    console.log(datenow);
+}
+
+date(printDate);
