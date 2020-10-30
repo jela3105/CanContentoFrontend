f1c5600ebb184db216f30d06f3bb94d9ce8491d6
jela3105 <jela3105@gmail.com>
2020-10-30 02:13:02 -0600

    add fetchData and promises

---
+++ src/Example/UtilsExample/fetchData.js
@@ -0,0 +1,21 @@
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+const fetchData = (url_api) =>{
+    return new Promise((resolve, reject) =>{
+        const xhttp = new XMLHttpRequest();
+        xhttp.open('GET', url_api, true);
+        xhttp.onreadystatechange = (() => {
+            //enlaces de estados: https://www.w3schools.com/xml/ajax_xmlhttprequest_response.asp
+            // el 4 indica que se completo la peticion
+            if(xhttp.readyState === 4){
+                (xhttp.status ===200)
+                ? resolve(JSON.parse(xhttp,this.responseText))
+                : reject(new Error('Error ', url_api))
+            }
+        });
+        xhttp.send();
+    });
+}
+
+//se utiliza module.exports porque se usa node
+module.exports = fetchData;
