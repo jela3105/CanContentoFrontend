993256895b4406bdf75f8fc90aea69540f5e8d5a
jela3105 <jela3105@gmail.com>
2020-11-10 20:32:51 -0600

    Add js script for google maps

--- Js/initialization.js
+++ Js/initialization.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     M.AutoInit()
 });
+
+function iniciarMap(){
+    var coord = {lat:-34.5956145 ,lng: -58.4431949};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
